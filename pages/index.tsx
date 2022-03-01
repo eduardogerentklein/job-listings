d69b4e6e71f61dd3271ac3c440e402897ea143e0
Eduardo Klein <eduardo.klein@fulltechpro.com.br>
2022-03-01 15:04:46 -0300

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.Ts!</a>
+          Welcome to <a href="https://nextjs.org">Next.ts!</a>
         </h1>
 
         <p className={styles.description}>
