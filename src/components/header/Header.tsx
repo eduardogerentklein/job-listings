21c522131c0230b212650890454546d67f717733
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2022-03-04 16:13:51 -0300

    refactor: components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,33 @@
+import { styled } from 'theme';
+
 type HeaderProps = {
   children?: JSX.Element | JSX.Element[];
 };
 
+const headerOptions = {
+  backgroundColor: '$primary',
+  width: '100%',
+  height: '156px',
+  '& img': {
+    content: 'url(./icons/bg-header-desktop.svg);'
+  },
+  '@small': {
+    '& img': {
+      content: 'url(./icons/bg-header-mobile.svg);'
+    }
+  }
+};
+const HeaderStyled = styled('header', headerOptions);
+
 export const Header = ({ children }: HeaderProps) => {
-  return <header>{children}</header>;
+  return (
+    <HeaderStyled>
+      <img
+        alt='Header image wave'
+        aria-label='Header image wave'
+        height='156px'
+      ></img>
+      {children}
+    </HeaderStyled>
+  );
 };
