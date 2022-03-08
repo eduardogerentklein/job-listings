bd83f4abdcacc602e5b1ec478ae4055b8ba2484d
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2022-03-08 17:29:18 -0300

    fix: theme configs

--- src/theme/theming.ts
+++ src/theme/theming.ts
@@ -4,7 +4,8 @@ export const Theme = {
     spartan: 'Spartan, sans-serif'
   },
   fontSizes: {
-    1: '15px'
+    1: '15px',
+    2: '17px'
   },
   fontWeights: {
     1: '500',
@@ -12,12 +13,27 @@ export const Theme = {
   },
   space: {
     1: '4px',
-    2: '12px'
+    2: '12px',
+    3: '16px',
+    4: '24px',
+    5: '28px',
+    6: '32px',
+    7: '35px',
+    8: '40px',
+    9: '45px',
+    10: '48px',
+    11: '56px',
+    12: '64px'
   },
   radii: {
     1: '4px',
     2: '8px',
     3: '32px'
+  },
+  shadows: {
+    1: '0 0 0 3px rgba(77, 100, 117, 0.25)',
+    2: '0px 16px 32px rgba(30, 42, 50, 0.08)',
+    3: '0px 6px 10px rgb(166 221 221)'
   },
   colors: {
     black: '#1E2A32',
