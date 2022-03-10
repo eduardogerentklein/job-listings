f7c41a762d92e0a2f2a27376e8fa12b55146f57a
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2022-03-10 17:01:28 -0300

    fix: typography utils colors

--- src/components/typography/utils/colors.ts
+++ src/components/typography/utils/colors.ts
@@ -3,7 +3,19 @@ export const Colors = {
   white: { color: '$neutralWhite' },
   lightGrayCyan10: { color: '$lightGrayCyan10' },
   lightGrayCyan20: { color: '$lightGrayCyan20' },
+  lightGrayCyan30: { color: '$lightGrayCyan30' },
   darkGrayCyan10: { color: '$darkGrayCyan10' },
   darkGrayCyan100: { color: '$darkGrayCyan100' },
   black: { color: '$black' }
+};
+
+export const BackgroundColors = {
+  primary: { backgroundColor: '$primary' },
+  white: { backgroundColor: '$neutralWhite' },
+  lightGrayCyan10: { backgroundColor: '$lightGrayCyan10' },
+  lightGrayCyan20: { backgroundColor: '$lightGrayCyan20' },
+  lightGrayCyan30: { backgroundColor: '$lightGrayCyan30' },
+  darkGrayCyan10: { backgroundColor: '$darkGrayCyan10' },
+  darkGrayCyan100: { backgroundColor: '$darkGrayCyan100' },
+  black: { backgroundColor: '$black' }
 };
