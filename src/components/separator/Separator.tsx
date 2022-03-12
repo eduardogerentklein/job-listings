ce55ce25d2ce09b644858a58de5e554eaf7b7746
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2022-03-12 18:48:06 -0300

    feat: creating separator component

---
+++ src/components/separator/Separator.tsx
@@ -0,0 +1,7 @@
+import * as SeparatorRadix from '@radix-ui/react-separator';
+
+import { styled } from 'theme';
+
+export const Separator = styled(SeparatorRadix.Root, {
+  backgroundColor: '$darkGrayCyan10'
+});
