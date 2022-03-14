a9446ef6349f55d904e461d2b01645e5822ff074
Eduardo Klein <eduardo.klein@catolicasc.org.br>
2022-03-14 17:28:02 -0300

    fix: change header image on mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,24 +9,26 @@ const headerOptions = {
   width: '100%',
   height: '156px',
   '& img': {
-    content: 'url(./icons/bg-header-desktop.svg);'
-  },
-  '@small': {
-    '& img': {
-      content: 'url(./icons/bg-header-mobile.svg);'
-    }
+    width: '100%'
   }
 };
 const HeaderStyled = styled('header', headerOptions);
 
 export const Header = ({ children }: HeaderProps) => {
+  const mobileImageUrl = './icons/bg-header-mobile-375w.svg 415w';
+  const desktopImageUrl = './icons/bg-header-desktop-1366w.svg';
   return (
     <HeaderStyled>
-      <img
-        alt='Header image wave'
-        aria-label='Header image wave'
-        height='156px'
-      ></img>
+      <picture>
+        <source media='(max-width: 415px)' srcSet={mobileImageUrl} />
+        <source media='(min-width: 800px)' srcSet={desktopImageUrl} />
+        <img
+          src={desktopImageUrl}
+          alt='Header image wave'
+          aria-label='Header image wave'
+          height='156px'
+        />
+      </picture>
       {children}
     </HeaderStyled>
   );
